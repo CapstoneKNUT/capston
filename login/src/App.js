210a08b6008124c16cee5a0ae24bddffec4b8ded
TaeWan-Kwon <119296420+TaeWan-Kwon@users.noreply.github.com>
2024-08-22 21:28:43 +0900

    메뉴바 레이아웃

--- login/src/App.js
+++ login/src/App.js
@@ -1,26 +1,26 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './layout/header.js';
-import profileimages from './회원.jpg';
-import settingimages from './설정.jpg';
+import Menubar from './Menubar'; // Menubar 컴포넌트 임포트
 
 function App() {
   return (
     <div className="App">
-    <div className='container'>
-      <div className='login-form'>
-        <label for="email">Email</label>
-        <input type='email' id='email' placeholder='Value'></input>
-        <label for='password'>Password</label>
-        <input type='password' id='password' placeholder='Value'></input>
-        <button type='button'>log in</button>
-        <div className='login-links'>
-          <a href='#'>Forgot password?</a>
-          <a href='#'>Sign up</a>
+      <Menubar />
+      <div className='container'>
+        <div className='login-form'>
+          <label htmlFor="email">Email</label>
+          <input type='email' id='email' placeholder='Value' />
+          <label htmlFor='password'>Password</label>
+          <input type='password' id='password' placeholder='Value' />
+          <button type='button'>Log in</button>
+          <div className='login-links'>
+            <a href='#'>Forgot password?</a>
+            <a href='#'>Sign up</a>
+          </div>
         </div>
+        <div className='login-image'></div>
       </div>
-      <div className='login-image'></div>
-    </div>
     </div>
   );
 }
--- login/src/Menubar.js
+++ login/src/Menubar.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import "./header.css";
 import profileimages from './회원.jpg';
 import settingimages from './설정.jpg';
+import './Menubar.css'; // 메뉴바 전용 CSS 파일 (선택 사항)
 
-function App() {
+function Menubar() {
   return (
-    <div className="App">
     <div className='menubar'>
       <div>🏠 Home</div>
       <div className='travel-info'>여행지 정보</div>
       <div className='travel-info'>리뷰 목록</div>
       <div className='travel-info'>마이 페이지</div>
       <div className='menu-icons'>
-        <img src={profileimages} alt='Profile' className='profile'></img>
-        <img src={settingimages} alt='Setting' className='setting'></img>
+        <img src={profileimages} alt='Profile' className='profile' />
+        <img src={settingimages} alt='Setting' className='setting' />
       </div>
-    </div>
     </div>
   );
 }
+
+export default Menubar;
