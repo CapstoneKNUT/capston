5f5f9a2087f08120f624efbdb2e657fc521dabf6
TaeWan-Kwon <119296420+TaeWan-Kwon@users.noreply.github.com>
2024-08-31 15:46:40 +0900

    지역추가

--- travel-info/src/data/regions.js
+++ travel-info/src/data/regions.js
@@ -9,7 +9,7 @@ export const regions = [
   },
   {
     name: "서울",
-    image: "https://kr.miceseoul.com/humanframe/theme/mice/assets/images/spot/img_spot_view01.png",
+    image: "https://50plus.or.kr/upload/im/2020/07/69e4299c-7f5a-4643-ab02-37aa0643f80d.jpg",
   },
   {
     name: "경주",
@@ -17,7 +17,23 @@ export const regions = [
   },
   {
     name: "청주",
-    image: "https://www.cj-ilbo.com/news/photo/202202/1073308_985928_357.jpg"
+    image: "https://www.cj-ilbo.com/news/photo/202202/1073308_985928_357.jpg",
   },
+  {
+    name: "충주",
+    image: "https://mediaim.expedia.com/destination/1/e2cc01d7154597ff25f1bdea2ea7ef48.jpg",
+  },
+  {
+    name: "전주",
+    image: "https://img.khan.co.kr/news/2020/05/16/l_2020051501001795300138001.jpg",
+  },
+  {
+    name: "나주",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz3I96plLxseGf7gGdrZaAR13P8oJm9XWNaQ&s",
+  },
+  {
+    name: "상주",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdATknCB3bkEmHgcDGDKbukOIpiFKCpLGxvA&s"
+  }
   // 추가적인 지역들을 여기에 나열
 ];
